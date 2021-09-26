80bc2e1a82cc0ef1c4e85ffea47103014e827051
Adel <shi.samadel@gmail.com>
2021-09-26 16:15:43 +0300

    bug fix and walking inner

--- core/alg_suzuki.py
+++ core/alg_suzuki.py
@@ -1,6 +1,8 @@
 import numpy as np
 import cv2 as cv
 
+
+
 def counterclockwise_walking(anchor_point, start_point=None):
     i, j = anchor_point
     points = [(i, j+1), (i-1, j+1), (i-1, j),
@@ -12,29 +14,46 @@ def counterclockwise_walking(anchor_point, start_point=None):
     for point in points:
         yield point
 
+
+def clockwise_walking(anchor_point, start_point=None):
+    i, j = anchor_point
+    points = [(i + 1, j + 1), (i + 1, j), (i + 1, j - 1), (i, j - 1), (i - 1, j - 1), (i - 1, j), (i - 1, j + 1), (i, j + 1),]
+
+    if start_point is not None:
+        idx_start_point = points.index(start_point)
+        points = points[idx_start_point + 1:] + points[:idx_start_point + 1]
+    for point in points:
+        yield point
+
 def algorithm_suzuki(mask):
-
-
+    marker = 1
     for i in range(mask.shape[0]):
         for j in range(1, mask.shape[1]):
+            start_point = None
             if mask[i, j - 1] == 0 and mask[i, j] == 1:
+                start_point = (i, j - 1)
+            elif mask[i, j] >= 1 and mask[i, j+1] == 0:
+                start_point = (i, j+1)
+            if start_point is not None:
                 anchor_point = (i, j)
                 init_points = None
                 last_searching_point = None
-                for searching_point in counterclockwise_walking(anchor_point):
-                    print(searching_point)
+
+                for searching_point in clockwise_walking(anchor_point, start_point):
                     if mask[searching_point] == 1:
                         init_points = (anchor_point, searching_point)
                         last_searching_point = searching_point
                         break
                 end = False
+                marker += 1
                 while not end:
+
                     for searching_point in counterclockwise_walking(anchor_point, last_searching_point):
                         if mask[searching_point] != 0:
                             if mask[anchor_point[0], anchor_point[1] + 1] != 0 and mask[anchor_point] == 1:
-                                mask[anchor_point] = 2
+                                mask[anchor_point] = marker
                             elif mask[anchor_point[0], anchor_point[1] + 1] == 0:
-                                mask[anchor_point] = -2
+                                mask[anchor_point] = -marker
 
 
                             if searching_point == init_points[0] and anchor_point == init_points[1]:
@@ -57,6 +76,8 @@ def algorithm_suzuki(mask):
 if __name__ == '__main__':
     mask = np.array([[0, 0, 0, 0, 0, 0, 0, 0],
                      [0, 0, 1, 1, 1, 1, 1, 0],
+                     [0, 0, 1, 1, 1, 1, 1, 0],
+                     [0, 0, 1, 1, 0, 1, 1, 0],
                      [0, 0, 1, 1, 1, 1, 1, 0],
                      [0, 0, 1, 1, 1, 1, 1, 0],
                      [0, 0, 0, 0, 0, 0, 0, 0]])

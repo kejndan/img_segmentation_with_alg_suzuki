9ec526abb293ba7c170cb3e9864d8c227c04df6e
Adel <shi.samadel@gmail.com>
2021-09-28 20:50:46 +0300

    add blur
    `

--- run.py
+++ run.py
@@ -4,30 +4,37 @@ import numpy as np
 
 from core.image import Image
 from core.alg_suzuki import algorithm_suzuki
-
+from icecream import ic
 
 class ImageSegmentation:
     def __init__(self, image):
-        self.lvl_lower = (40, 20, 30)
-        self.lvl_upper = (75, 190, 240)
+        self.lvl_lower = (40, 30, 35)
+        self.lvl_upper = (75, 215, 230)
+
+
 
         self.image = image
+        self.image_rgb = cv.cvtColor(cv.imread('segment.jpg'), cv.COLOR_BGR2RGB)
 
     def __select__object(self):
+        self.image.img = cv.GaussianBlur(self.image.img, (17,17), 100)
         mask_hue = cv.inRange(self.image.img, self.lvl_lower, self.lvl_upper)
 
+
+        ic(np.unique(mask_hue))
         mask_hue = np.where(mask_hue == 255, 1, mask_hue)
-        print(mask_hue.max())
-        mask = algorithm_suzuki(mask_hue)
-        print(mask)
+        #
+        ic(np.unique(mask_hue))
+
+        mask,_ = algorithm_suzuki(mask_hue.astype(np.int32))
         for i in range(mask_hue.shape[0]):
             for j in range(mask_hue.shape[1]):
-                if mask[i, j] == 2:
+                if mask[i, j] != 0 and mask[i,j] != 1:
                     self.image.img[i, j] = np.array([255, 255, 255])
                 else:
                     self.image.img[i, j] = np.array([0, 0, 0])
                 print(mask_hue[i,j])
-        # self.image.img = cv.bitwise_and(self.image.img, self.image.img, mask=mask_hue)
+        self.image.img = cv.bitwise_and(self.image_rgb, self.image_rgb, mask=mask_hue)
 
     def run(self):
         self.__select__object()
